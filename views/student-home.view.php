e8fa893618acfe92ea63b0fe344ada6f5ecb1b8f
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-06-12 09:53:02 +0600

    component based

--- views/student-home.view.php
+++ views/student-home.view.php
@@ -9,30 +9,7 @@
         <div class="main_content">
             <div class="class_list_cards">
                 <?php foreach ($classes as $class) : ?>
-
-                    <a href="/class-details?id=<?php echo $class['id'] ?>">
-                        <div class="wrapper">
-                            <div class="block">
-                                <div class="cover">
-                                    <div class="cover-photo">
-                                    </div>
-                                    <div class="classroom-info">
-                                        <p class="class-name"> <?= $class['name'] ?> </p>
-                                        <p><?= $class['teacher_name'] ?></p>
-                                        <p><?= $class['section'] ?></p>
-                                    </div>
-                                    <div class="round-circle">
-                                        <img src="https://i.pravatar.cc/70" alt="">
-                                    </div>
-                                </div>
-
-                                <div class="extra-icons">
-                                    <i class="bi bi-person"></i>
-                                    <i class="bi bi-folder2"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
+                    <?php include 'partials/class-card.php' ?>
                 <?php endforeach; ?>
             </div>
         </div>
